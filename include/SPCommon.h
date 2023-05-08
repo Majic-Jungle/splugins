6f568d68c4b55b127fac561ed1f4c1c1e858329d
Dave Frampton <davidframpton@majicjungle.com>
2023-05-09 09:36:32 +1200

    add user data to custom options. Update Windows lib files to current versions.

--- include/SPCommon.h
+++ include/SPCommon.h
@@ -11,6 +11,13 @@ typedef struct SPWorldGenOptions {
 	double temperatureOffset;
 	SPVec3 scales;
 	SPVec3 influences;
+
+	SPVec4 userDataA; //these can all be used for mods that want to add more options. They are read from the table passed through from Lua.
+	SPVec4 userDataB;
+	SPVec4 userDataC;
+	SPVec4 userDataD;
+	void* userDataP; //any data can be stored in here by mods. On the Lua side, it needs to be passed in the customOptions table as a lua string for the key 'userDataP'. A good way to do this might be to serialize your own struct using LuaJIT's FFI
+
 } SPWorldGenOptions;
 
 
